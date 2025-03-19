7331a100cf2e726dc360701828a05f671f7a2748
DylanPower25 <dylan.szapiro@gmail.com>
2025-03-19 18:26:20 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const tituloCancion = document.querySelector('.reproductor h1');
+const nombreArtista = document.querySelector('.reproductor p');
+
+const progreso = document.getElementById('progreso');
+const cancion = document.getElementById('cancion');
